53bee352de198cffa2db5d4a0365a3844b38b044
Nikos Tsiknakis <tsiknakisn@gmail.com>
2017-10-31 08:05:43 +0200

    calc + gui added
    Basic CalculatorGui jar added.
    Initial Calc.class added

---
+++ RPN_Calc/src/calculator/Calc.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class Calc {
+
+	public static void main(String args[]) {
+		//CalculatorGui calc = new CalculatorGui();
+		System.out.println("Hello World");
+	}
+
+}
